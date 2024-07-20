ce0f4c9c7e8fc8d8bfeef03188c58e75a65dad4b
Yash Dewangan <yashpd13@gmail.com>
2024-07-20 13:33:58 +0530

    updated pay-z

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,12 @@
-import { PrismaClient } from "@repo/db/client";
-const client = new PrismaClient();
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
-export default function Home() {
-  return <div className="text-2xl">Hello world</div>;
+export default function Page(): JSX.Element {
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
